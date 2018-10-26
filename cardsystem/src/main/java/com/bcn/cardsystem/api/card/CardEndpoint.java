94672d34c1847f8bdd7f21e88a89cf720696c221
Muyinda Rogers <moverr@gmail.com>
2018-10-26 10:14:07 +0300

    CardEndpoint :

---
+++ cardsystem/src/main/java/com/bcn/cardsystem/api/card/CardEndpoint.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bcn.cardsystem.api.card;
+
+/**
+ *
+ * @author mover 
+ */
+public class CardEndpoint {
+    
+}
