b4390b2a1e22982f171dbfaec8586f987aed526b
Muyinda Rogers <moverr@gmail.com>
2018-10-26 10:28:54 +0300

    Debugigng :

--- cardsystem/src/main/java/com/bcn/cardsystem/api/card/CardEndpoint.java
+++ cardsystem/src/main/java/com/bcn/cardsystem/api/card/CardEndpoint.java
@@ -5,10 +5,31 @@
  */
 package com.bcn.cardsystem.api.card;
 
+import com.bcn.cardsystem.api.card.entities.Card;
+import com.bcn.cardsystem.api.card.entities.CardResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+
 /**
  *
- * @author mover 
+ * @author mover
  */
 public class CardEndpoint {
-    
+
+    //todo: post card details through api :: 
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public CardResponse validate_checksum(Card entity,
+            @HeaderParam("authentication") String authentication,
+            @Context HttpServletRequest httpRequest) throws Exception {
+        CardResponse cr = new CardResponse();
+        return cr;
+    }
+
 }
