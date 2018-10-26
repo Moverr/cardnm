4c70cd0e21460df415533026bed1176a619ada68
Muyinda Rogers <moverr@gmail.com>
2018-10-26 10:16:53 +0300

    Card Entities :

---
+++ cardsystem/src/main/java/com/bcn/cardsystem/api/card/entities/Card.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bcn.cardsystem.api.card.entities;
+
+import com.bcn.cardsystem.annotation.Mandatory;
+import java.util.Objects;
+import org.dom4j.tree.AbstractEntity;
+
+/**
+ *
+ * @author mover
+ */
+public class Card  extends AbstractEntity {
+    private Integer id;
+    private @Mandatory
+    String number;
+
+    public Card() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 17 * hash + Objects.hashCode(this.id);
+        hash = 17 * hash + Objects.hashCode(this.number);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Card other = (Card) obj;
+        if (!Objects.equals(this.number, other.number)) {
+            return false;
+        }
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Card{"
+                + "id=" + id 
+                + ", number=" + number 
+                + '}';
+    }
+    
+    
+    
+    
+    
+    
+}
